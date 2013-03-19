7ede8922526dc520456df515691b756c551b3f31
Jack Dubie <jack@orchestra.com>
2013-03-18 22:32:34 -0700

    Done

--- Makefile
+++ Makefile
@@ -7,4 +7,8 @@ build:
 	gcc main.c -o main.out
 
 run:
-	./main.out
+	./main.out 0  | diff s0.out.gold  -
+	./main.out 1  | diff s1.out.gold  -
+	./main.out 2  | diff s2.out.gold  -
+	./main.out 3  | diff s3.out.gold  -
+	./main.out 5  | diff s5.out.gold  -
--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
-int main (void)
+void
+spiral(int side, int offset)
 {
-  printf("Hello World");
+  /*
+   * Base case: empty square
+   */
+  if (side == 0) return;
+
+  /*
+   * Base case: unit square
+   */
+  if (side == 1) {
+    printf("(%d, %d)\n", offset, offset);
+    return;
+  }
+
+  int i, j = 0;
+  for (i = 0; i < side - 1; i++) printf("(%d, %d)\n", offset + i, offset + j);
+  for (j = 0; j < side - 1; j++) printf("(%d, %d)\n", offset + i, offset + j);
+  for (; i > 0; i--)             printf("(%d, %d)\n", offset + i, offset + j);
+  for (; j > 0; j--)             printf("(%d, %d)\n", offset + i, offset + j);
+
+  spiral(side - 2, offset + 1);
+}
+
+int main (int argv, char *args[])
+{
+  int n = atoi(args[1]);
+  spiral(n, 0);
   return 0;
 }
